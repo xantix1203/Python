ccfa33677eec3502c0bf88b600e1b54434e1669d
xantix1203 <39125050+xantix1203@users.noreply.github.com>
2020-04-16 20:00:14 +0200

    travail Grille.py

--- Bataille navale/Grille.py
+++ Bataille navale/Grille.py
@@ -5,12 +5,14 @@ class Grille:
     def __init__(self, species):
         self.grille = [[0] * 10 for i in range(10)]
         if species == "bot":
-            for i in range(2, 6):
+            for i in range(5, 1, -1):
                 for j in range(6 - i):
                     flag = True
                     while flag:
                         x, y, alpha = randint(0, 9), randint(0, 9), randint(0, 1)
-                        for i in range()
+                        for i in range(4):
+                            if i == 0:
+                                if self.grille[]
 
 
 
