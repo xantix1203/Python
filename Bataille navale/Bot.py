b17cc4b8db1555933ef2c43f62378a65c11fbce9
xantix1203 <39125050+xantix1203@users.noreply.github.com>
2020-04-17 09:04:11 +0200

    update bots' name

--- Bataille navale/Bot.py
+++ Bataille navale/Bot.py
@@ -3,4 +3,4 @@ from Player import *
 
 class Bot(Player):
     def __init__(self, score=0):
-        Player.__init__(self, choice(["Ken", "Barbie", "Mickael", "Ta mère", "Joe la classe", "Samsoum"]), "bot", score)
+        Player.__init__(self, choice(["Ken", "Barbie", "Mickael", "Joe la classe", "Samsoum"]), "bot", score)
