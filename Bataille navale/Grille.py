855e73276450e8f6e828de9de73dde8e0dcc363e
xantix1203 <39125050+xantix1203@users.noreply.github.com>
2020-04-17 08:45:23 +0200

    générateur de grilles

--- Bataille navale/Grille.py
+++ Bataille navale/Grille.py
@@ -9,28 +9,30 @@ class Grille:
             for i in range(5, 1, -1):
                 for j in range(6 - i):
                     flag = True
-                    while flag:
+                    m = 0
+                    while flag and m < 1000:
                         x, y, alpha = randint(0, 9), randint(0, 9), randint(0, 1)
                         for k in range(4):
-                            if k == 0:
-                                if y + i < 10 and self.grille[x][y:y + i + 1] == [0] * i:
+                            if (k + alpha) % 4 == 0:
+                                if y + i < 10 and (self.grille[x, y:y + i] == np.zeros(i)).all():
                                     flag = False
-                                    self.grille[x][y:y + i + 1] = [1] * i
+                                    self.grille[x, y:y + i] = [i] * i
                                     break
-                            if k == 1:
-                                if x + i < 10 and self.grille[x: x + i + 1][y] == [0] * i:
+                            if k + alpha == 1:
+                                if x + i < 10 and (self.grille[x: x + i, y] == np.zeros(i)).all():
                                     flag = False
-                                    self.grille[x:x + i + 1][y] = [1] * i
+                                    self.grille[x:x + i, y] = [i] * i
                                     break
-                            if k == 2:
-                                if y - i >= 0 and self.grille[x][y - i:y + 1] == [0] * i:
+                            if k + alpha == 2:
+                                if y - i >= 0 and (self.grille[x, y - i:y] == np.zeros(i)).all():
                                     flag = False
-                                    self.grille[x][y - i:y + 1] = [1] * i
+                                    self.grille[x, y - i:y] = [i] * i
                                     break
-                            if k == 3:
-                                if x - i >= 0 and self.grille[x - i:x + 1][y] == [0] * i:
+                            if (k + alpha) == 3:
+                                if x - i >= 0 and (self.grille[x - i:x, y] == np.zeros(i)).all():
                                     flag = False
-                                    self.grille[x - i:x + 1][y] = [1] * i
+                                    self.grille[x - i:x, y] = [i] * i
+                        m += 1
 
     def __str__(self):
         return str(self.grille)
