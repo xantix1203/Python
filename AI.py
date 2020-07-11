8888d9421cab95078aaffd45dadebb416236ad90
xantix1203 <39125050+xantix1203@users.noreply.github.com>
2020-07-11 13:07:56 +0200

    class Neural Network

--- AI.py
+++ AI.py
@@ -1,1 +1,11 @@
+import numpy as np
+
+
 class NeuralNetwork:
+    def __init__(self,
+                 input_size=3,
+                 output_size=1,
+                 hidden_size=3):
+        self.inputSize = input_size
+        self.outputSize = output_size
+        self.hiddenSize = hidden_size
