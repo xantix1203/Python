4084ed900f68236a7fbbac9e52511f89150a6e1d
xantix1203 <39125050+xantix1203@users.noreply.github.com>
2020-07-25 12:14:14 +0200

    backpropagation

--- AI/AI.py
+++ AI/AI.py
@@ -11,14 +11,43 @@ class NeuralNetwork:
 
     def feed_forward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoide(np.dot(w, a) + b)
+            a = sigmoid(np.dot(w, a) + b)
         return a
 
-    def update_mini_batch(self, mini_batche, eta):
+    def backprop(self, x, y):
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * \
+                sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+    def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The "mini_batch" is a list of tuples "(x, y)", and "eta"
         is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
 
     def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         """Train the neural network using mini-batch stochastic
@@ -40,6 +69,11 @@ class NeuralNetwork:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
                 print("Epoch {0}: {1} / {2}").format(
-                    j, self.evaluate(test_data), n_test)
+                    j, self.evaluate(test_data), n_test)  # TODO write evaluate
             else:
                 print("Epoch {0} complete").format(j)
+
+    @staticmethod
+    def cost_derivative(output_activations, y):
+        """Return the vector of partial derivatives."""
+        return output_activations - y
--- AI/functions.py
+++ AI/functions.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 
-def sigmoide(x):
+def sigmoid(x):
+    """the sigmoid function"""
     return 1.0 / (1.0 + np.exp(-x))
+
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z) * (1 - sigmoid(z))
